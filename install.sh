db6c5bebbf9ff752b9610925e1e59ea12579b210
Timm-png <timmpng45@gmail.com>
2021-02-07 23:32:06 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme
+git clone https://github.com/vinceliuice/McMojave-cursors
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme
+
+./McMojave-cursors/install.sh  
+./WhiteSur-icon-theme/install.sh
+./WhiteSur-gtk-theme/install.sh 

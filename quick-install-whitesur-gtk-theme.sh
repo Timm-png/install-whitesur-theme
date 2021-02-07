981799043edf0774086885b4a92b372da092d682
Timm-png <timmpng45@gmail.com>
2021-02-07 23:41:25 +0200

    Add autoremove foldersd

--- quick-install-whitesur-gtk-theme.sh
+++ quick-install-whitesur-gtk-theme.sh
@@ -5,3 +5,7 @@ git clone https://github.com/vinceliuice/WhiteSur-icon-theme
 ./McMojave-cursors/install.sh  
 ./WhiteSur-icon-theme/install.sh
 ./WhiteSur-gtk-theme/install.sh 
+
+rm -r McMojave-cursors
+rm -r WhiteSur-gtk-theme
+rm -r WhiteSur-icon-theme
